39f4e7827453d69424e16605da693539b11e634f
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-28 13:02:58 +0100

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,5 +1,6 @@
 base_de_datos = {}
 x = 1
+z = 0
 
 while x == 1:
     print("introduzca 1, para añadir alumno (alumno:aprobado/suspenso)\n"
@@ -7,7 +8,13 @@ while x == 1:
           "introduzca 3, para mostrar toda la clase")
     y = int(input("¿Que desea?"))
     if y == 1:
-        nombre = input("añadir alumno (alumno:aprobado/suspenso)")
+        nombre = input("añadir alumno (alumno:aprobado/suspenso)\n"
+                       "todo en minusculas porfavor")
         a, b = nombre.split(":")
         base_de_datos[a] = b
-    elif y == 2: 
+    elif y == 2:
+        g = list(base_de_datos.values())
+        t = g.count("true")       
+        print("El numero de aprobados es:",t)
+    elif y == 3:
+        print(base_de_datos)
