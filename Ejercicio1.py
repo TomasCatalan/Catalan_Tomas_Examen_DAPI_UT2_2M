af5b6c8384e325709c39c5c140486347a32a3766
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-28 13:46:52 +0100

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,26 @@
+
+    
+nombres = ["jose miguel", "aitor menta", "benito camelas"]
+
+def OrdenarNombre(nombre_Apellido):
+    
+    """Funcion: recibe un nombre y un apellido y devuelve apellido nombre
+    
+        - Parametros:
+            - Nombre en el formato: Nombre Apellido
+            
+        - Salida:
+            - Nombre en el formato: Apellido, Nombre"""
+    x = nombre_Apellido.split(" ")
+    apellido = x[-1]
+    nombre = " ".join(x[:-1])
+
+    nombre_ordenado = (apellido, nombre)
+
+    print(nombre_ordenado)
+    return nombre_ordenado
+
+
 def ListaNombre(nombre_apellido_a):
     
     """Funcion: la funcion recibe una lista y la ordena por apellidos en 
@@ -10,7 +33,11 @@ def ListaNombre(nombre_apellido_a):
 
         - Salida:
             - Devuelve una lista de nombres ordenados alfabeticamente:
-            
+            ["Apellido, Nombre 1", ... "Apellido, Nombre N"]
+              """
+    
+    nombres.sort(key = OrdenarNombre)
+    
+    return nombres
 
-                
-                """
+ListaNombre(nombres)
