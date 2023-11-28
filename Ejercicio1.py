371724351459ee39b83c18c0e8680e0d20fdd63d
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-28 13:12:50 +0100

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,16 @@
+def ListaNombre(nombre_apellido_a):
+    
+    """Funcion: la funcion recibe una lista y la ordena por apellidos en 
+                orden alfabetico
+         
+        - Parametros:
+            - Lista de nombres: esta lista guarda una cantidad indeterminada
+            de nombres:
+                ["Nombre Apellido 1", ..."Nombre apellido N"]
+
+        - Salida:
+            - Devuelve una lista de nombres ordenados alfabeticamente:
+            
+
+                
+                """
